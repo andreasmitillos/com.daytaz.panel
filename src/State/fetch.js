60e89b0ebdd7884ac1bd4196c0af64da4d3c4f11
andreasmitillos <mitillos.andreas@gmail.com>
2023-08-20 19:40:22 +0100

    Assignment of Options Lists to Items

--- src/State/fetch.js
+++ src/State/fetch.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// const API_URL = "http://127.0.0.1:3002/api";
+const API_URL = "http://127.0.0.1:3002/api";
 // const API_URL = "http://192.168.1.193:3002/api";
-const API_URL = "http://192.168.0.4:3002/api";
+// const API_URL = "http://192.168.0.4:3002/api";
 
 let fetchApi = (type, path, data) => {
   return new Promise((resolve, reject) => {
     let temp = { data, method: type, withCredentials: true };
 
-    if (type == "get") {
+    if (type === "get") {
       temp.params = data;
     }
 
