5d074f344b71bca267dd225cabcadba7dfd0c7ee
andreasmitillos <mitillos.andreas@gmail.com>
2023-08-29 16:40:02 +0300

    Changes to menu details - name and description

--- src/Screens/Menus/OtherMenu.jsx
+++ src/Screens/Menus/OtherMenu.jsx
@@ -7,6 +7,7 @@ import GeneralTable from "../../Components/Table/GeneralTable";
 import LoadingBox from "../../Components/LoadingBox";
 import Modal from "../../Components/Modal/Modal";
 import DynamicForm from "../../Components/Forms/DynamicForm";
+import { current } from "@reduxjs/toolkit";
 
 const CreateAllergyTagModal = (props) => {
   let { menu } = props;
@@ -174,6 +175,108 @@ const CreateDietaryRestrictionModal = (props) => {
   );
 };
 
+const EditMenuDetailsModal = (props) => {
+  let { menu, nameChange, descriptionChange } = props;
+
+  let { menuId } = useParams();
+
+  const [forceClose, setForceClose] = useState(0);
+
+  const toggleModal = () => setForceClose(forceClose + 1);
+
+  const callBack = (response, values) => {
+    if (response.status.code === "edit_menu_success") {
+      toggleModal();
+    }
+  };
+
+  let fields = [];
+
+  if (nameChange) {
+    fields.push({
+      labelRight: "Optional",
+      key: "name",
+      type: "text",
+      label: "New Menu Name",
+      placeholder: menu.name,
+    });
+  }
+
+  if (descriptionChange) {
+    fields.push({
+      labelRight: "Optional",
+      key: "description",
+      type: "textarea",
+      label: "New Menu Description",
+      placeholder: menu.description,
+    });
+  }
+
+  return (
+    <Modal
+      size="lg"
+      forceClose={forceClose}
+      // buttonText={"Edit Details"}
+      // buttonSize={"sm"}
+      // buttonVariant={"blue"}
+      triggerComponent={
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={2}
+          stroke="currentColor"
+          className="w-4 h-4 ml-1 text-blue-600 dark:text-blue-500 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      }
+    >
+      <div className="flex flex-col items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-12 h-12 mb-4 dark:text-blue-500 text-blue-600"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+
+        <p className="text-lg font-semibold dark:text-white">
+          Edit Menu Details
+        </p>
+        <p className="text-sm text-slate-500 dark:text-slate-400 text-center mt-2">
+          You are about to make changes to the details of{" "}
+          <span className={"font-semibold"}>{menu.name}</span>
+        </p>
+
+        <div className="w-full mt-6">
+          <DynamicForm
+            button="Save Changes"
+            buttonVariant="blue"
+            buttonCallBack={callBack}
+            buttonOnClick={menus.actions.editMenu}
+            fields={fields}
+            hiddenValues={{
+              menuId,
+            }}
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
 const OtherMenu = (props) => {
   // get current menu id
   const { menuId } = useParams();
@@ -213,6 +316,7 @@ const OtherMenu = (props) => {
   subscribe(menus.data, () => {
     setDietaryRestrictions(menus.data.menuDietaryRestrictions[menuId]);
     setAllergyTags(menus.data.menuAllergyTags[menuId]);
+    setCurrentMenu(menus.data.menu[menuId]);
     setUpdate(update + 1);
   });
 
@@ -305,6 +409,45 @@ const OtherMenu = (props) => {
               ])}
             />
           )}
+        </div>
+      </div>
+
+      <div className="col-span-6">
+        <h2 className="text-2xl font-extrabold">General Details</h2>
+
+        <div className="grid grid-cols-6 lg:gap-12 gap-6 mt-3 mb-6">
+          <div className="lg:col-span-1 col-span-6">
+            {/* <GeneralCard title={"Details"}>asd</GeneralCard> */}
+            <div className="flex items-center text-slate-600 text-sm font-semibold dark:text-slate-400">
+              <p>Name</p>
+
+              <EditMenuDetailsModal menu={currentMenu} nameChange />
+            </div>
+
+            <p className="text-sm font-semibold ">
+              {currentMenu.name}
+              {/*<input*/}
+              {/*  className="outline-none focus:border-b border-dashed bg-transparent w-full"*/}
+              {/*  value={currentMenu.name}*/}
+              {/*/>*/}
+            </p>
+          </div>
+
+          <div className="lg:col-span-2 col-span-6">
+            {/* <GeneralCard title={"Details"}>asd</GeneralCard> */}
+            <div className="flex items-center text-slate-600 text-sm font-semibold dark:text-slate-400">
+              <p>Description</p>
+
+              <EditMenuDetailsModal menu={currentMenu} descriptionChange />
+            </div>
+            <p className="text-sm font-semibold h-full">
+              {/*<textarea*/}
+              {/*  className="outline-none border-b dark:border-slate-600 border-dashed bg-transparent w-full h-full"*/}
+              {/*  value={currentMenu.description}*/}
+              {/*/>*/}
+              {currentMenu.description}
+            </p>
+          </div>
         </div>
       </div>
     </MenuTemplate>
